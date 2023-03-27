43a4f6e0ae204ce23fbfc33fb840ff347da67265
Alexey <a.g.1.6@mail.ru>
2023-03-27 15:57:21 +0700

    updated

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import flask
 from flask import request, render_template, url_for
+from geopy.distance import geodesic
+from geopy.geocoders import Nominatim
 
 app = flask.Flask(__name__)
 
 infocity = {
-    'moscow': {
+    'msk': {
         'content': 'Арарат Парк Хаятт.\n''Один из лучших отелей Москвы, который подарит вам незабываемые впечатления от посещения этого '
                    'города.\n ''40т. рублей за ночь',
         'img': 'msk.png',
         'content1': 'Хаятт Плейc. Крутой пятизвездочный отель прямо в центре Москвы.\n 35т. рублей за ночь',
         'img1': 'ekb.png',
         'content2': 'AZIMUT Сити.\n ''Один из отелей Екатеринбурга, достойный быть названным лучшим.\n ''50т. рублей за ночь',
-        "img2": 'spb.png'
+        "img2": 'spb.png',
+        "name" : "Moscow"
     },
-    'ekaterinburg': {
+    'ekb': {
         'content': 'Хаятт Плейc. Крутой пятизвездочный отель прямо в центре Екатеринбурга.\n 35т. рублей за ночь',
         'img': 'ekb.png',
         'content1': 'AZIMUT Сити.\n ''Один из отелей Екатеринбурга, достойный быть названным лучшим.\n ''50т. рублей за ночь',
@@ -21,9 +24,10 @@ infocity = {
         'content2': 'Арарат Парк Хаятт.\n''Один из лучших отелей Екатеринбурга, который подарит вам незабываемые '
                     'впечатления от посещения этого'
                    'города.\n ''40т. рублей за ночь',
-        'img2': 'msk.png'
+        'img2': 'msk.png',
+        "name" : "Ekaterinburg"
     },
-    'sanktptb': {
+    'spb': {
         'content': 'AZIMUT Сити.\n ''Один из отелей Санкт-Петербурга, достойный быть названным лучшим.\n ''50т. рублей за ночь',
         "img": 'spb.png',
         'content1': 'Хаятт Плейc. Крутой пятизвездочный отель прямо в центре СПб.\n 35т. рублей за ночь',
@@ -31,9 +35,20 @@ infocity = {
         'content2': 'Арарат Парк Хаятт.\n''Один из лучших отелей Екатеринбурга, который подарит вам незабываемые '
                     'впечатления от посещения этого'
                    'города.\n ''40т. рублей за ночь',
-        'img2': 'msk.png'
+        'img2': 'msk.png',
+        "name" : "Saint-Petersburg"
+    },
+    'nsk' : {
+        "name" : "Novosibirsk",
+    },
+    'irk' : {
+        "name" : "Irkutsk"
+    },
+    'bar' : {
+        "name" : "Barnaul"
     }
 }
+
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -43,31 +58,23 @@ def index():
 
 @app.route('/content')
 def content():
-    option = request.args.get('selector')
-    if option == 'msk':
-        content = infocity['moscow']['content']
-        img = infocity['moscow']['img']
-        content1 = infocity['moscow']['content1']
-        img1 = infocity['moscow']['img1']
-        content2 = infocity['moscow']['content2']
-        img2 = infocity['moscow']['img2']
-    elif option == 'ekb':
-        content = infocity['ekaterinburg']['content']
-        img = infocity['ekaterinburg']['img']
-        content1 = infocity['ekaterinburg']['content1']
-        img1 = infocity['ekaterinburg']['img1']
-        content2 = infocity['ekaterinburg']['content2']
-        img2 = infocity['ekaterinburg']['img2']
-    elif option == 'spb':
-        content = infocity['sanktptb']['content']
-        img = infocity['sanktptb']['img']
-        content1 = infocity['sanktptb']['content1']
-        img1 = infocity['sanktptb']['img1']
-        content2 = infocity['sanktptb']['content2']
-        img2 = infocity['sanktptb']['img2']
-    else:
-        content = None
-    return render_template('index.html', content=content, img=img, content1=content1, img1=img1, content2=content2, img2=img2)
+    option = request.args.get('selector1')
+    option1 = request.args.get('selector')
+    name0 = infocity['{0}'.format(option1)]['name']
+    name1 = infocity['{0}'.format(option)]['name']
+    content = infocity['{0}'.format(option)]['content']
+    img = infocity['{0}'.format(option)]['img']
+    content1 = infocity['{0}'.format(option)]['content1']
+    img1 = infocity['{0}'.format(option)]['img1']
+    content2 = infocity['{0}'.format(option)]['content2']
+    img2 = infocity['{0}'.format(option)]['img2']
+    geolocator = Nominatim(user_agent="my_app")
+    location1 = geolocator.geocode(infocity['{0}'.format(option1)]['name'])
+    location2 = geolocator.geocode(infocity['{0}'.format(option)]['name'])
+    distance = int(geodesic((location1.latitude, location1.longitude), (location2.latitude, location2.longitude)).km)
+    vremya = int(distance / 360)
+    price = distance * 3
+    return render_template('index.html', content=content, img=img, content1=content1, img1=img1, content2=content2, img2=img2, distance=distance, vremya=vremya, price = price, name0=name0, name1=name1)
 
 
 if __name__ == "__main__":
